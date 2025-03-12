f46b6299641a4dfac690781667b72a984cae6b70
Mathieu-ballout <mathieu.hego01@gmail.com>
2025-03-12 09:26:03 +0100

    Add files via upload
    quelques bugs avec notamment la couleur des boules

---
+++ connect.php
@@ -0,0 +1,5 @@
+<?php
+	    define ('SERVEUR_BD','localhost');
+	    define ('LOGIN_BD','root');
+	    define ('PASS_BD','');
+	    define ('NOM_BD','test'); 
---
+++ connexion.php
@@ -0,0 +1,19 @@
+<?php
+    require('../connect.php');
+	
+	$CONNEXION = mysqli_connect (SERVEUR_BD, LOGIN_BD, PASS_BD);
+	//Connexion au serveur de bases de données
+	if (mysqli_connect_errno()) {
+		echo 'Désolé, connexion au serveur ' . SERVEUR_BD . ' impossible, '. mysqli_connect_error(), "\n";
+    	exit();
+	}
+	// Sélection de la base de données
+	mysqli_select_db($CONNEXION, NOM_BD);
+	if (mysqli_connect_errno()) {
+		echo 'Désolé, accès à la base ' . NOM_BD . ' impossible, '. mysqli_connect_error(), "\n";
+    	exit();
+	}
+	// Spécification de l'encodage UTF-8 pour dialoguer avec la BD
+	if (!mysqli_set_charset($CONNEXION, 'UTF8')) {
+    	echo 'Erreur au chargement de l\'encodage UTF-8 : ', mysqli_connect_error(), "\n";
+	}
