4541e29e5ddd555ba89f2dfa12077543f9cb2b0c
naja2444 <147331697+naja2444@users.noreply.github.com>
2025-04-09 08:48:22 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,14 @@
 <?php
-define ('db_host', getenv('SERVEUR_BD'))
-define ('db_user', getenv('LOGIN_BD'))
-define ('db_password', getenv('PASS_BD'))
-define ('db_db', getenv('NOM_BD'))
+//define ('db_host', getenv('SERVEUR_BD'))
+//define ('db_user', getenv('LOGIN_BD'))
+//define ('db_password', getenv('PASS_BD'))
+//define ('db_db', getenv('NOM_BD'))
+
+$db_host = "mariadb";
+$db_db = "casino";
+$db_password = "mathieu";
+$db_user = "mathieu";
+
 try {
     // Connexion à la base de données avec PDO
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_db;charset=utf8", $db_user, $db_password);
